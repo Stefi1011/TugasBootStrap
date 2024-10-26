ba4f8a2abf4d8e93d82c9e18376344fee66aa71d
Stefi1011 <153803440+Stefi1011@users.noreply.github.com>
2024-10-26 23:13:48 +0700

    [FIX] Membetulkan script

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Untuk contact agar tombol hanya bisa diklik bila semua data sudah terisi dan meresetnya setelah tombol diklik
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+event.preventDefault(); 
+
+if (this.checkValidity()) {
+    var emailModal = new bootstrap.Modal(document.getElementById('emailModal'), {});
+    emailModal.show();
+    this.reset();
+} else {
+    this.reportValidity();
+}
+});
+
+
 // Untuk mengisi membership type dan price secara otomatis
 document.addEventListener('DOMContentLoaded', function() {
     const membershipButtons = document.querySelectorAll('.membership-button');
